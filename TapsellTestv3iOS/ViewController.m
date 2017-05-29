a95970956848b24fb58640dd47871e1821720056
Tapsell <tapsell@Tapsells-MacBook-Pro.local>
2017-05-29 18:05:40 +0430

    Initial Commit

--- TapsellTestv3iOS/ViewController.m
+++ TapsellTestv3iOS/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import <TapsellSDKv3/TapsellSDKv3.h>
+#import <Foundation/Foundation.h>
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    TSConfiguration* config = [[TSConfiguration alloc] init];
+    [config setDebugMode:YES];
+    [Tapsell initializeWithAppKey:@"ljrdhscbkpjrkslfljrjpgtbjjfdajgrlihaocnqsnplgmsckssqkogsfmfatlbjelbfcd" andConfig:config];
+    
+    //NSString* vastUrl = [TapsellVAST getVastUrlForZone:@"5913110746846551e1340acf" withType:PrerollTypeShort ofVastVersion:VAST3];
+    //NSLog(@"%@", [NSString stringWithFormat:@"vastUrl: %@",vastUrl]);
+    
+    TSAdRequestOptions* requestOptions = [[TSAdRequestOptions alloc] init];
+    [requestOptions setCacheType:CacheTypeCached];
+    
+    [Tapsell setAdShowFinishedCallback:^(TapsellAd *ad, BOOL completed) {
+        if([ad isRewardedAd] && completed)
+        {
+            NSLog(@"Congratulations! 1 coin awarded.");
+        }
+    }];
+    
+    [Tapsell requestAdForZone:@"5913110746846551e1340acf"
+                   andOptions:requestOptions
+                onAdAvailable:^(TapsellAd *ad){
+                    NSLog(@"AdAvailable");
+                    TSAdShowOptions* showOptions = [[TSAdShowOptions alloc] init];
+                    [showOptions setOrientation:OrientationUnlocked];
+                    [showOptions setBackDisabled:YES];
+                    [showOptions setShowDialoged:YES];
+                    [ad showWithOptions:showOptions];
+                }
+              onNoAdAvailable:^{
+                  NSLog(@"NoAdAvailable");
+              }
+                      onError:^(NSString* error){
+                          NSLog(@"%@", [NSString stringWithFormat:@"No ad available, error:%@",error]);
+                      }
+                   onExpiring:^(TapsellAd *ad){
+                       NSLog(@"Expiring");
+                   }];
 }
 
 
